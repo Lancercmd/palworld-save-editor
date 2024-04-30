16b1047bfa8094547fc5c6c3de67895f4c474bb2
Lancercmd <lancercmd@gmail.com>
2024-04-30 08:23:59 +0800

    ⚗️ Technical preview
    - 实现对帕鲁信息的修改。

--- L10N.py
+++ L10N.py
@@ -222,4 +222,200 @@ LOCALE = {
         "en": " Please keep a backup in case of data loss.",
         "zh_Hans": "请注意保留备份，以防数据丢失。",
     },
+    "Edit Pal": {
+        "en": "Edit Pal",
+        "zh_Hans": "编辑帕鲁",
+    },
+    "Click to switch": {
+        "en": "Click to switch",
+        "zh_Hans": "点击切换",
+    },
+    "Rank": {
+        "en": "Rank",
+        "zh_Hans": "浓缩等级",
+    },
+    "Rank HP": {
+        "en": "Rank HP",
+        "zh_Hans": "最大 HP 强化等级",
+    },
+    "Rank Attack": {
+        "en": "Rank Attack",
+        "zh_Hans": "攻击 强化等级",
+    },
+    "Rank Defense": {
+        "en": "Rank Defense",
+        "zh_Hans": "防御 强化等级",
+    },
+    "Rank CraftSpeed": {
+        "en": "Rank CraftSpeed",
+        "zh_Hans": "工作速度 强化等级",
+    },
+    "Equip Waza": {
+        "en": "Equip Waza",
+        "zh_Hans": "装备技能",
+    },
+    "Mastered Waza": {
+        "en": "Mastered Waza",
+        "zh_Hans": "已掌握技能",
+    },
+    "Edit": {
+        "en": "Edit",
+        "zh_Hans": "编辑",
+    },
+    "Editor": {
+        "en": "Editor",
+        "zh_Hans": "编辑器",
+    },
+    "Ranks MAX": {
+        "en": "Ranks MAX",
+        "zh_Hans": "浓缩等级和强化等级 MAX",
+    },
+    "Talents MAX": {
+        "en": "Talents MAX",
+        "zh_Hans": "个体值 MAX",
+    },
+    "Edit Equip Waza": {
+        "en": "Edit Equip Waza",
+        "zh_Hans": "编辑装备技能",
+    },
+    "Edit Mastered Waza": {
+        "en": "Edit Mastered Waza",
+        "zh_Hans": "编辑已掌握技能",
+    },
+    "Equip Waza cannot have more than 4.": {
+        "en": "Equip Waza cannot have more than 4.",
+        "zh_Hans": "装备技能不能超过 4 个。",
+    },
+    "Waza ID": {
+        "en": "Waza ID",
+        "zh_Hans": "技能 ID",
+    },
+    "Enter Waza ID": {
+        "en": "Enter Waza ID",
+        "zh_Hans": "输入技能 ID",
+    },
+    "Duplicate Waza ID is not allowed.": {
+        "en": "Duplicate Waza ID is not allowed.",
+        "zh_Hans": "不允许重复的技能 ID。",
+    },
+    "Waza ID is invalid.": {
+        "en": "Waza ID is invalid.",
+        "zh_Hans": "技能 ID 无效。",
+    },
+    "Remove Selected": {
+        "en": "Remove Selected",
+        "zh_Hans": "删除选中",
+    },
+    "Remove All": {
+        "en": "Remove All",
+        "zh_Hans": "删除所有",
+    },
+    "Modified": {
+        "en": "Modified",
+        "zh_Hans": "已修改",
+    },
+    "Character ID cannot be empty.": {
+        "en": "Character ID cannot be empty.",
+        "zh_Hans": "帕鲁 ID 不能为空。",
+    },
+    "Character ID is invalid.": {
+        "en": "Character ID is invalid.",
+        "zh_Hans": "帕鲁 ID 无效。",
+    },
+    "Rank must be a number.": {
+        "en": "Rank must be a number.",
+        "zh_Hans": "浓缩等级必须是数字。",
+    },
+    "Rank cannot be higher than 5.": {
+        "en": "Rank cannot be higher than 5.",
+        "zh_Hans": "浓缩等级不能高于 5。",
+    },
+    "Rank HP must be a number.": {
+        "en": "Rank HP must be a number.",
+        "zh_Hans": "最大 HP 强化等级必须是数字。",
+    },
+    "Rank HP cannot be higher than 10.": {
+        "en": "Rank HP cannot be higher than 10.",
+        "zh_Hans": "最大 HP 强化等级不能高于 10。",
+    },
+    "Rank Attack must be a number.": {
+        "en": "Rank Attack must be a number.",
+        "zh_Hans": "攻击 强化等级必须是数字。",
+    },
+    "Rank Attack cannot be higher than 10.": {
+        "en": "Rank Attack cannot be higher than 10.",
+        "zh_Hans": "攻击 强化等级不能高于 10。",
+    },
+    "Rank Defense must be a number.": {
+        "en": "Rank Defense must be a number.",
+        "zh_Hans": "防御 强化等级必须是数字。",
+    },
+    "Rank Defense cannot be higher than 10.": {
+        "en": "Rank Defense cannot be higher than 10.",
+        "zh_Hans": "防御 强化等级不能高于 10。",
+    },
+    "Rank CraftSpeed must be a number.": {
+        "en": "Rank CraftSpeed must be a number.",
+        "zh_Hans": "工作速度 强化等级必须是数字。",
+    },
+    "Rank CraftSpeed cannot be higher than 10.": {
+        "en": "Rank CraftSpeed cannot be higher than 10.",
+        "zh_Hans": "工作速度 强化等级不能高于 10。",
+    },
+    "Talent HP must be a number.": {
+        "en": "Talent HP must be a number.",
+        "zh_Hans": "个体：HP 必须是数字。",
+    },
+    "Talent HP cannot be higher than 100.": {
+        "en": "Talent HP cannot be higher than 100.",
+        "zh_Hans": "个体：HP 不能高于 100。",
+    },
+    "Talent Melee must be a number.": {
+        "en": "Talent Melee must be a number.",
+        "zh_Hans": "个体：近战 必须是数字。",
+    },
+    "Talent Melee cannot be higher than 100.": {
+        "en": "Talent Melee cannot be higher than 100.",
+        "zh_Hans": "个体：近战 不能高于 100。",
+    },
+    "Talent Shot must be a number.": {
+        "en": "Talent Shot must be a number.",
+        "zh_Hans": "个体：远程 必须是数字。",
+    },
+    "Talent Shot cannot be higher than 100.": {
+        "en": "Talent Shot cannot be higher than 100.",
+        "zh_Hans": "个体：远程 不能高于 100。",
+    },
+    "Talent Defense must be a number.": {
+        "en": "Talent Defense must be a number.",
+        "zh_Hans": "个体：防御 必须是数字。",
+    },
+    "Talent Defense cannot be higher than 100.": {
+        "en": "Talent Defense cannot be higher than 100.",
+        "zh_Hans": "个体：防御 不能高于 100。",
+    },
+    "Edit Passive Skills": {
+        "en": "Edit Passive Skills",
+        "zh_Hans": "编辑被动技能",
+    },
+    "Passive Skill ID": {
+        "en": "Passive Skill ID",
+        "zh_Hans": "被动技能 ID",
+    },
+    "Passive Skills cannot have more than 4.": {
+        "en": "Passive Skills cannot have more than 4.",
+        "zh_Hans": "被动技能不能超过 4 个。",
+    },
+    "Enter Passive Skill ID": {
+        "en": "Enter Passive Skill ID",
+        "zh_Hans": "输入被动技能 ID",
+    },
+    "Passive Skill ID is invalid.": {
+        "en": "Passive Skill ID is invalid.",
+        "zh_Hans": "被动技能 ID 无效。",
+    },
+    "Duplicate Passive Skill ID is not allowed.": {
+        "en": "Duplicate Passive Skill ID is not allowed.",
+        "zh_Hans": "不允许重复的被动技能 ID。",
+    },
 }
